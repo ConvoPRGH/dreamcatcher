97408516ec8e4d043b6dc723583e01d1c2ba0fa7
watzmonium <ixisixi@gmail.com>
2023-09-11 18:04:17 -0400

    started backend config

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+const config = require('./utils/config');
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => `listening on port${config.PORT}`)

58307809debe805bc4c2f2ef09787ad251fccb61
watzmonium <ixisixi@gmail.com>
2023-09-12 17:01:36 -0400

    installed mongodb

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-// const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
-  PORT
+  PORT,
+  MONGODB_URI
 }

d47d6bb220013a29216db5f482756e9009d25e3d
frye-t <tyler.frye42@gmail.com>
2023-09-12 18:17:39 -0400

    added dynamic post route

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/bins/', binsRouter)
 
-
 console.log(`connecting to ${config.MONGODB_URI}`);
 (async () => {
   try {
@@ -26,6 +25,20 @@ console.log(`connecting to ${config.MONGODB_URI}`);
 })();
 
 // app.use(express.static(''))
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.post('/bins/:bin_path', (req, res) => {
+  const binPath = req.params.bin_path;
+  console.log(`Got a request to ${binPath}`);
+  console.log(req.method);
+  console.log(req.path);
+  console.log(req.body);
+  res.status(201).send('POST request successful')
+})
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

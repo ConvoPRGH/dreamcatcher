b9bf4300bf08f9de78014362159fc25651857550
frye-t <tyler.frye42@gmail.com>
2023-09-12 19:34:52 -0400

    file restructure, js loading js

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,10 @@ app.use(express.static(path.join(__dirname, '../frontend/public'), { "extensions
 
 
 console.log(path.join(__dirname, '../frontend/src'));
-// app.use(cors());
-// app.use(express.json())
-// app.use(middleware.requestLogger)
-// app.use('/api/bins/', binsRouter)
+app.use(cors());
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/bins/', binsRouter)
 
 
 console.log(`connecting to ${config.MONGODB_URI}`);
@@ -37,16 +37,16 @@ app.get('/', (req, res) => {
   res.sendFile(filePath)
 });
 
-// app.post('/bins/:bin_path', (req, res) => {
-//   const binPath = req.params.bin_path;
-//   console.log(`Got a request to ${binPath}`);
-//   console.log(req.method);
-//   console.log(req.path);
-//   console.log(req.body);
-//   res.status(201).send('POST request successful')
-// })
+app.post('/bins/:bin_path', (req, res) => {
+  const binPath = req.params.bin_path;
+  console.log(`Got a request to ${binPath}`);
+  console.log(req.method);
+  console.log(req.path);
+  console.log(req.body);
+  res.status(201).send('POST request successful')
+})
 
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = app;

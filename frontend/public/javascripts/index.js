e7e71a9e5d4bd7bdc072830526912c8db2db3d4e
watzmonium <ixisixi@gmail.com>
2023-09-12 20:04:40 -0400

    basic functionality

--- frontend/public/javascripts/index.js
+++ frontend/public/javascripts/index.js
@@ -1,17 +1,31 @@
 import Bin from '../src/classes/Bin';
 import Request from '../src/classes/Request';
-
+import TemplateManager from '../src/classes/TemplateManager';
 import Seed from '../src/utils/Seed';
 
 const seed = new Seed();
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('index.js loaded');
-  console.log(seed.getOneBin());
-  console.log(seed.getBins());
-  console.log(seed.getOneRequest());
-  console.log(seed.getRequests());
+  const manager = new TemplateManager;
+  populateLists(manager.templates.all_bins);
+  console.log(manager.templates)
+  // console.log('index.js loaded');
+  // console.log(seed.getOneBin());
+  // console.log(seed.getBins());
+  // console.log(seed.getOneRequest());
+  // console.log(seed.getRequests());
 });
+
+const populateLists = async (all_bins) => {
+  try {
+    const list = document.querySelector('#bin-list')
+    const response = await fetch('http://localhost:3000/api/bins')
+    const data = await response.json()
+    list.innerHTML = all_bins({bin: data})
+  } catch(e) {
+    console.log(e.message)
+  }
+};
 
 // console.log(seed.getBins());
 // Bin {

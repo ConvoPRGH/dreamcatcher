612ab7c456cdf1e9c71b368a94c900c1570f7bd1
watzmonium <ixisixi@gmail.com>
2023-09-13 19:21:17 -0400

    blew out reqs

--- frontend/public/src/classes/Request.js
+++ frontend/public/src/classes/Request.js
@@ -1,10 +1,16 @@
 export default class Request {
   constructor(requestObject) {
     this.id = requestObject.id;
-    this.mongoId = requestObject.mongoId;
-    this.receivedAt = requestObject.receivedAt;
-    this.httpMethod = requestObject.httpMethod;
-    this.httpPath = requestObject.httpPath;
+    this.mongo_id = requestObject.mongo_id;
+    this.received_at = this.#mapDatetoYMDT(requestObject.received_at)
+    this.http_method = requestObject.http_method;
+    this.http_path = requestObject.http_path;
     this.payload = requestObject.payload;
+  }
+
+  #mapDatetoYMDT(date) {
+    const formatDate = new Date(date);
+    const stdDate = String(formatDate).split(' G')[0];
+    return stdDate;
   }
 }
